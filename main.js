b557bfcbee6dc04d8f44af06e824e3a032123dcb
laeeqah <laeeqahesau@gmail.com>
2020-11-23 13:21:30 +0200

    done all changes

--- main.js
+++ main.js
@@ -1,7 +1,63 @@
-function myNewfunction(){
-    document.getElementsByClassName(txt).innerHTML = "cal.value"
+function number(num){
+    console.log(num)
+    document.getElementById("txt").value += num;
 
 }
+
+function displayResults(){
+    let x = document.getElementById("txt").value;
+    let y = eval(x);
+    document.getElementById("txt").value = y;
+
+}
+
+function clearAll(){
+    document.getElementById("txt").value = "";
+}
+
+function backspace(){
+    let x = document.getElementById("txt").value;
+    let a = x.slice(0, -1)
+    document.getElementById("txt").value = a;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
